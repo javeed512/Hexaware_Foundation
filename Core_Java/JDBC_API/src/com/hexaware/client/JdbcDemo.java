bcb210ee717c882b36c9f96dd6573061dcbe6326
javeed512 <javeedmohammedjaveed@gmail.com>
2024-10-10 11:33:44 +0530

    JDBC Intro , Demo with Statement DML , notes,diagram

---
+++ Core_Java/JDBC_API/src/com/hexaware/client/JdbcDemo.java
@@ -0,0 +1,40 @@
+package com.hexaware.client;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcDemo {
+
+	public static void main(String[] args) {
+
+		try {
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "admin");
+
+			Statement stmt = conn.createStatement();
+
+			String insert = "insert into Employees values(112,'smith',70000)";
+
+			//String update = "update Employees set ename = 'javeed khan' , salary = 99000 where eid = 111   ";
+			
+			 // String delete = "delete from Employees where eid = 111";
+			
+			
+			int count = stmt.executeUpdate(insert);
+			// it is used for DML , return integer which mean no. of records   effected
+
+			System.out.println(count +" records effected");
+			
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
