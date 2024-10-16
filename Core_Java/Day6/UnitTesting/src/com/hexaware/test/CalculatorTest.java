f1e3b7602a75073e4b48373740f40fe716b3b8b3
javeed512 <javeedmohammedjaveed@gmail.com>
2024-10-16 10:14:47 +0530

    Unit Testing annotations , EMS service Tests , PDFs

--- Core_Java/Day6/UnitTesting/src/com/hexaware/test/CalculatorTest.java
+++ Core_Java/Day6/UnitTesting/src/com/hexaware/test/CalculatorTest.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +17,45 @@ import com.hexaware.app.Calculator;
 
 class CalculatorTest {
 
-	Calculator cal = new Calculator();
+	static Calculator cal = null;
+	
+	@BeforeAll
+	public static void  beforeAll() {
+		
+		cal = new Calculator();
+		
+		System.out.println("Before All executed...");
+		//any initialization work like creating objects, getting DB Conn , read files
+	}
+	
+	
+	@AfterAll
+	public static void afterAll() {
+		
+		// post construct code like cleaning memory , close files , close DB Conn etc
+		
+		System.out.println("After All executed..");
+		
+		
+		
+	}
+	
+	
+	
+	@BeforeEach
+	public void  init() {
+		
+		System.out.println("Before Each..");
+		
+	}
+	
+	@AfterEach
+	public void  destroy() {
+		
+		System.out.println("After Each..");
+		
+	}
+	
 
 	@Test
 	@DisplayName("Add")
@@ -22,6 +64,8 @@ class CalculatorTest {
 		int actual = cal.add(15, 5);
 
 		assertEquals(20, actual);
+		
+		System.out.println("add test");
 
 	}
 
@@ -33,7 +77,7 @@ class CalculatorTest {
 			int actual =  cal.sub(25, 5);
 		
 				assertTrue(actual > 0);
-		
+				System.out.println("sub test");
 	}
 
 	@Test
@@ -45,6 +89,8 @@ class CalculatorTest {
 				
 			assertNotEquals(res, 5);
 
+			System.out.println("mul test");
+			
 	}
 
 	@Test
@@ -54,6 +100,7 @@ class CalculatorTest {
 			
 				assertFalse(res < 0);
 		
+				System.out.println("div test");
 
 	}
 
---
+++ Core_Java/UnitTesting/src/com/hexaware/test/DemoTest.java
@@ -0,0 +1,77 @@
+package com.hexaware.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.hexaware.app.Calculator;
+
+class DemoTest {
+	
+	static Calculator cal = null;
+	
+	@BeforeAll // @BeforeClass
+	public static void  beforeAll() {
+		
+		cal = new Calculator();
+		
+		System.out.println("Before All executed...");
+		//any initialization work like creating objects, getting DB Conn , read files
+	}
+	
+	
+
+	@ParameterizedTest
+	@ValueSource(strings = {"king","tom","black smith","javeed"})
+	void test1(String name) {
+		
+		
+				assertTrue( name.length() > 1);
+				
+				System.out.println("test1");
+		
+		
+	}
+	
+	@DisplayName(value = "(:")
+	@RepeatedTest(value = 5)
+	public void  test2() {
+		
+		
+		
+			assertEquals(10, cal.add(5, 5));
+		
+			System.out.println("test2");
+		
+	}
+	
+	@Test
+	public void test3() {
+		
+		
+		assertTrue(true);
+		System.out.println("test3");
+	}
+	
+	
+	
+	@AfterAll //  AfterClass
+	public static void afterAll() {
+		
+		// post construct code like cleaning memory , close files , close DB Conn etc
+		
+		System.out.println("After All executed..");
+		
+	
+	}
+	
+	
+	
+
+}
